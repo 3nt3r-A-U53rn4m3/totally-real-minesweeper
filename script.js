1aef46c1cc0f2b8a8228fdb08f29a9781dda85fe
3nt3r-A-U53rn4m3 <108638101+3nt3r-A-U53rn4m3@users.noreply.github.com>
2022-12-31 09:50:48 -0800

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,9 @@ bombscount = 0;
 flagscount = 10;
 i = 1;
 
-//
+//bruh I had to do this project with no console
+//like the conosle stopped working
+//basically think of python project with no print()
 
 boxes = document.getElementById("boxesarea2");
 
@@ -186,6 +188,7 @@ function bye() {
   g.style.display = "none";
 };
 
+//this part doesn't work bc idk
 function rollbye() {
   r = document.getElementById("rollarea");
   r.style.display = "none";
@@ -193,3 +196,47 @@ function rollbye() {
 }
 
 makegrid(grid);
+
+dragElement(document.getElementById("gridarea"))
+dragElement(document.getElementById("rollarea"))
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id)) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id).onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
